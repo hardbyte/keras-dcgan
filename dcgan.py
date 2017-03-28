d8c2bc2b22dc7ac5fe61a9869fdc012a5c9809be
Brian Thorne <brian@thorne.link>
2017-03-29 01:21:38 +1100

    Work with either tensorflow or theano image order
    Update to keras 2 api
    Save weights every epoch
    Save a smaller snapshot of generated images
    Add a few more arguments

--- dcgan.py
+++ dcgan.py
@@ -1,13 +1,19 @@
+"""
+Based on https://github.com/jacobgil/keras-dcgan
+
+"""
+
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Reshape
 from keras.layers.core import Activation
 from keras.layers.normalization import BatchNormalization
-from keras.layers.convolutional import UpSampling2D
-from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.layers.convolutional import UpSampling2D, Conv2D
+from keras.layers.convolutional import MaxPooling2D
 from keras.layers.core import Flatten
 from keras.optimizers import SGD
 from keras.datasets import mnist
+from keras import backend as K
 import numpy as np
 from PIL import Image
 import argparse
@@ -16,30 +22,25 @@ import math
 
 def generator_model():
     model = Sequential()
-    model.add(Dense(input_dim=100, output_dim=1024))
+    model.add(Dense(units=1024, input_dim=100))
     model.add(Activation('tanh'))
     model.add(Dense(128*7*7))
     model.add(BatchNormalization())
     model.add(Activation('tanh'))
-    model.add(Reshape((128, 7, 7), input_shape=(128*7*7,)))
+    model.add(Reshape((7, 7, 128), input_shape=(128*7*7,)))
     model.add(UpSampling2D(size=(2, 2)))
-    model.add(Convolution2D(64, 5, 5, border_mode='same'))
-    model.add(Activation('tanh'))
+    model.add(Conv2D(64, kernel_size=5, padding='same', activation='tanh'))
     model.add(UpSampling2D(size=(2, 2)))
-    model.add(Convolution2D(1, 5, 5, border_mode='same'))
-    model.add(Activation('tanh'))
+    model.add(Conv2D(1, kernel_size=5, padding='same', activation='tanh'))
     return model
 
 
-def discriminator_model():
+def discriminator_model(input_shape):
     model = Sequential()
-    model.add(Convolution2D(
-                        64, 5, 5,
-                        border_mode='same',
-                        input_shape=(1, 28, 28)))
+    model.add(Conv2D(64, kernel_size=5, padding='same', input_shape=input_shape))
     model.add(Activation('tanh'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Convolution2D(128, 5, 5))
+    model.add(Conv2D(128, kernel_size=5))
     model.add(Activation('tanh'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Flatten())
@@ -62,22 +63,40 @@ def combine_images(generated_images):
     num = generated_images.shape[0]
     width = int(math.sqrt(num))
     height = int(math.ceil(float(num)/width))
-    shape = generated_images.shape[2:]
+    shape = (28, 28)
     image = np.zeros((height*shape[0], width*shape[1]),
                      dtype=generated_images.dtype)
     for index, img in enumerate(generated_images):
         i = int(index/width)
         j = index % width
-        image[i*shape[0]:(i+1)*shape[0], j*shape[1]:(j+1)*shape[1]] = \
-            img[0, :, :]
+        if K.image_data_format() == 'channels_first':
+            image[i*shape[0]:(i+1)*shape[0], j*shape[1]:(j+1)*shape[1]] = \
+                img[0, :, :]
+        else:
+            image[i*shape[0]:(i+1)*shape[0], j*shape[1]:(j+1)*shape[1]] = \
+                img[:, :, 0]
     return image
 
 
-def train(BATCH_SIZE):
-    (X_train, y_train), (X_test, y_test) = mnist.load_data()
-    X_train = (X_train.astype(np.float32) - 127.5)/127.5
-    X_train = X_train.reshape((X_train.shape[0], 1) + X_train.shape[1:])
-    discriminator = discriminator_model()
+def train(batch_size, epochs, save_epoch_weights, path=''):
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    # input image dimensions
+    img_rows, img_cols = 28, 28
+    # Reshape
+    if K.image_data_format() == 'channels_first':
+        print("Backend prefers channels first")
+        x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+        x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+        input_shape = (1, img_rows, img_cols)
+    else:
+        print("Channels last")
+        x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+        x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+        input_shape = (img_rows, img_cols, 1)
+
+    x_train = (x_train.astype(np.float32) - 127.5)/127.5
+
+    discriminator = discriminator_model(input_shape)
     generator = generator_model()
     discriminator_on_generator = \
         generator_containing_discriminator(generator, discriminator)
@@ -88,44 +107,51 @@ def train(BATCH_SIZE):
         loss='binary_crossentropy', optimizer=g_optim)
     discriminator.trainable = True
     discriminator.compile(loss='binary_crossentropy', optimizer=d_optim)
-    noise = np.zeros((BATCH_SIZE, 100))
-    for epoch in range(100):
-        print("Epoch is", epoch)
-        print("Number of batches", int(X_train.shape[0]/BATCH_SIZE))
-        for index in range(int(X_train.shape[0]/BATCH_SIZE)):
-            for i in range(BATCH_SIZE):
+    noise = np.zeros((batch_size, 100))
+    print("{} batches per epoch".format(x_train.shape[0] // batch_size))
+
+    for epoch in range(epochs):
+        for index in range(int(x_train.shape[0]/batch_size)):
+            for i in range(batch_size):
                 noise[i, :] = np.random.uniform(-1, 1, 100)
-            image_batch = X_train[index*BATCH_SIZE:(index+1)*BATCH_SIZE]
+            image_batch = x_train[index * batch_size:(index + 1) * batch_size]
             generated_images = generator.predict(noise, verbose=0)
-            if index % 20 == 0:
-                image = combine_images(generated_images)
+            if index % 50 == 0:
+                image = combine_images(generated_images[:25])
                 image = image*127.5+127.5
-                Image.fromarray(image.astype(np.uint8)).save(
-                    str(epoch)+"_"+str(index)+".png")
+                filename = '{}/{}_{}.png'.format(path, epoch, index)
+                Image.fromarray(image.astype(np.uint8)).save(filename)
+
+            # Compute the discriminator's loss using the generated images:
             X = np.concatenate((image_batch, generated_images))
-            y = [1] * BATCH_SIZE + [0] * BATCH_SIZE
+            y = [1] * batch_size + [0] * batch_size
             d_loss = discriminator.train_on_batch(X, y)
-            print("batch %d d_loss : %f" % (index, d_loss))
-            for i in range(BATCH_SIZE):
+            for i in range(batch_size):
                 noise[i, :] = np.random.uniform(-1, 1, 100)
             discriminator.trainable = False
+
             g_loss = discriminator_on_generator.train_on_batch(
-                noise, [1] * BATCH_SIZE)
+                noise, [1] * batch_size)
             discriminator.trainable = True
-            print("batch %d g_loss : %f" % (index, g_loss))
-            if index % 10 == 9:
-                generator.save_weights('generator', True)
-                discriminator.save_weights('discriminator', True)
+            if index % 10 == 0:
+                print("Epoch {} batch:{:3} d_loss: {:8.6f} g_loss: {:8.6f}".format(epoch, index, d_loss, g_loss))
+
+        if save_epoch_weights:
+            generator.save_weights('generator_{}.h5'.format(epoch), True)
+            discriminator.save_weights('discriminator_{}.h5'.format(epoch), True)
+
+        generator.save_weights('generator.h5'.format(epoch), True)
+        discriminator.save_weights('discriminator.h5'.format(epoch), True)
 
 
 def generate(BATCH_SIZE, nice=False):
     generator = generator_model()
     generator.compile(loss='binary_crossentropy', optimizer="SGD")
-    generator.load_weights('generator')
+    generator.load_weights('generator.h5')
     if nice:
         discriminator = discriminator_model()
         discriminator.compile(loss='binary_crossentropy', optimizer="SGD")
-        discriminator.load_weights('discriminator')
+        discriminator.load_weights('discriminator.h5')
         noise = np.zeros((BATCH_SIZE*20, 100))
         for i in range(BATCH_SIZE*20):
             noise[i, :] = np.random.uniform(-1, 1, 100)
@@ -155,15 +181,21 @@ def generate(BATCH_SIZE, nice=False):
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--mode", type=str)
-    parser.add_argument("--batch_size", type=int, default=128)
+    parser.add_argument("--path", type=str, help="Path to save generated images")
+    parser.add_argument("--batch-size", type=int, default=128)
+    parser.add_argument("--epochs", type=int, default=100)
+    parser.add_argument("--save-weights", dest="save_epochs", action='store_true',
+                        help="Save weights for each epoch")
     parser.add_argument("--nice", dest="nice", action="store_true")
-    parser.set_defaults(nice=False)
+    parser.set_defaults(nice=False, save_epochs=False)
     args = parser.parse_args()
     return args
 
 if __name__ == "__main__":
     args = get_args()
     if args.mode == "train":
-        train(BATCH_SIZE=args.batch_size)
+        import os.path
+        path = os.path.abspath(args.path)
+        train(batch_size=args.batch_size, epochs=args.epochs, save_epoch_weights=args.save_epochs, path=path)
     elif args.mode == "generate":
         generate(BATCH_SIZE=args.batch_size, nice=args.nice)
